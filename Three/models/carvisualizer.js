d21dea8abcdf20b13ca6234c91f626c460997461
pparkssi <pparkssi@naver.com>
2015-12-24 18:01:53 +0900

    Three.js car model

---
+++ Three/models/carvisualizer.js
@@ -0,0 +1,230 @@
+{
+
+"metadata" :
+{
+	"formatVersion" : 3.2,
+	"type"          : "scene",
+	"sourceFile"    : "",
+	"generatedBy"   : "Blender 2.65 Exporter",
+	"objects"       : 10,
+	"geometries"    : 10,
+	"materials"     : 0,
+	"textures"      : 0
+},
+
+"urlBaseType" : "relativeToScene",
+
+
+"objects" :
+{
+	"ferrari_interior" : {
+		"geometry"  : "geo_ferrari_interior",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+    
+    "ferrari_glass" : {
+		"geometry"  : "geo_ferrari_glass",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"ferrari_bumper" : {
+		"geometry"  : "geo_ferrari_bumper",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+    
+    "ferrari_body" : {
+		"geometry"  : "geo_ferrari_body",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"wheel" : {
+		"geometry"  : "geo_wheel",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"rim" : {
+		"geometry"  : "geo_rim",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+    
+    "garage" : {
+		"geometry"  : "geo_garage",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"floor" : {
+		"geometry"  : "geo_floor",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"car_shadow" : {
+		"geometry"  : "geo_car_shadow",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	},
+
+	"floor_shadow" : {
+		"geometry"  : "geo_floor_shadow",
+		"groups"    : [  ],
+		"material"  : "",
+		"position"  : [ 0, 0, 0 ],
+		"rotation"  : [ 1.97149e-07, 0, -0 ],
+		"quaternion": [ 9.85746e-08, 0, 0, 1 ],
+		"scale"     : [ 1, 1, 1 ],
+		"visible"       : true,
+		"castShadow"    : false,
+		"receiveShadow" : false,
+		"doubleSided"   : false
+	}
+},
+
+
+"geometries" :
+{
+	"geo_ferrari_interior" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.ferrari_interior.js"
+	},
+    
+    "geo_ferrari_glass" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.ferrari_glass.js"
+	},
+
+	"geo_ferrari_bumper" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.ferrari_bumper.js"
+	},
+    
+    "geo_ferrari_body" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.ferrari_body.js"
+	},
+
+	"geo_wheel" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.wheel.js"
+	},
+    
+    "geo_rim" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.rim.js"
+	},
+
+	"geo_garage" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.garage.js"
+	},
+
+	"geo_floor" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.floor.js"
+	},
+
+	"geo_car_shadow" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.car_shadow.js"
+	},
+
+	"geo_floor_shadow" : {
+		"type" : "ascii",
+		"url"  : "carvisualizer.floor_shadow.js"
+	}
+},
+
+
+"transform" :
+{
+	"position"  : [ 0, 0, 0 ],
+	"rotation"  : [ 0, 0, 0 ],
+	"scale"     : [ 1, 1, 1 ]
+},
+
+"defaults" :
+{
+	"bgcolor" : [ 0, 0, 0 ],
+	"bgalpha" : 1.000000,
+	"camera"  : ""
+}
+
+}
